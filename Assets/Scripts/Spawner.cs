7f11f8c179451fe88b79cc32566ac2497d7d8a6d
NiamNjam <146223019+NiamNjam@users.noreply.github.com>
2024-02-07 20:01:10 +0200

    food async

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,16 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class FoodEntry
+{
+    public bool isBomb;
+    public float x;
+    public float delay;
+    public bool isRandomPosition;
+    public Vector2 velocity = new Vector2(0, 10);
+}
+[System.Serializable]
+public class Wave
+{
+    public List<FoodEntry> foods;
+    
+}
+
 public class Spawner : MonoBehaviour
 {
     public float spawnSpeed = 1f;
     public int bombChance = 20;
     public GameObject fruitPrefab;
     public GameObject bomb;
+    public int currentWave;
+    public List<Wave> waves;
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        InvokeRepeating("SpawnFruit", 0f, spawnSpeed);
+        while (currentWave < waves.Count)
+        {
+            
+            var wave = waves[currentWave];
+           
+            for (int i = 0; i < wave.foods.Count; i++)
+            {
+                var food = wave.foods[i];
+                await new WaitForSeconds(food.delay);
+
+                var prefab = food.isBomb ? bomb : fruitPrefab;
+                var go = Instantiate(prefab);
+                var x = food.isRandomPosition ? Random.Range(-5f, 5f) : food.x;
+
+                go.transform.position = new Vector3(x, -5, 0);
+                go.GetComponent<Rigidbody2D>().velocity = food.velocity;
+            }
+               
+            
+            await new WaitForSeconds(3f);
+            currentWave++;
+        }
     }
     void Update()
     {
@@ -21,11 +60,12 @@ public class Spawner : MonoBehaviour
             Destroy(fruitPrefab);
         }
     }
-
+    /*
     void SpawnFruit()
     {
         var prefab = Random.Range(0, 100) > bombChance ? fruitPrefab : bomb;
         int fruitpos = Random.Range(-5, 5);
         Instantiate(prefab, new Vector3(fruitpos, -5, 0), Quaternion.identity);
     }
+    */
 }
